6289ddd8ae7fc1663394aaa29d106820bba2b7ac
aciucaru <aciukru@gmail.com>
2025-06-11 21:32:22 +0300

    Removed old DAOs

--- src/main/java/inventory/tracking/dao/DocumentDataDAO.java
+++
@@ -1,6 +0,0 @@
-package inventory.tracking.dao;
-
-public class DocumentDataDAO extends AbstractCrudDAO<DocumentDataEntity, Long>
-{
-    public DocumentDataDAO() { super(DocumentDataEntity.class); }
-}
